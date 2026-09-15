ead3f637df7c2c68f327392e44adc43cb8806b5a
Megha <mardhi.meghana@gmail.com>
2026-09-15 21:14:24 +0530

    test fixed workflow with Google Client ID

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// test auto-deploy
+// test auto-deploy v2
 import { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
